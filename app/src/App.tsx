0aa984d3bc6cd4640d8285280c1409dc57830ff1
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-16 15:57:15 -0500

    Add simple counter for React app.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header flex flex-col space-y-4">
         <div className="font-black text-4xl">Learn React</div>
+        <div className="flex place-items-center space-x-2">
+          <button onClick={() => setCount(count - 1)} className="border p-2">
+            -
+          </button>
+          <div>{count}</div>
+          <button onClick={() => setCount(count + 1)} className="border p-2">
+            +
+          </button>
+        </div>
       </header>
     </div>
   );
