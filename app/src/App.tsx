1ff73584ddd41482ca807a6e66d924a61c6d0b2e
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-16 15:50:29 -0500

    Remove image link.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,14 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <div className="font-black text-4xl">Learn React</div>
-        </a>
+        <div className="font-black text-4xl">Learn React</div>
       </header>
     </div>
   );
