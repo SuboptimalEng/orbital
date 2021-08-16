945e14786e3fdeadc073b9d5f1661f0d49759963
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-16 17:14:49 -0500

    Add statemanagement with redux and typescript.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState } from './redux/store';
+import { decrement, increment } from './redux/counterSlice';
 
 function App() {
+  // Component state
   const [count, setCount] = useState(0);
-
   const incrementCount = () => {
     setCount(count + 1);
   };
-
   const decrementCount = () => {
     setCount(count - 1);
   };
+
+  // Redux state
+  const value = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
@@ -23,6 +29,15 @@ function App() {
           </button>
           <div>{count}</div>
           <button onClick={incrementCount} className="border p-2">
+            +
+          </button>
+        </div>
+        <div className="flex place-items-center space-x-2">
+          <button onClick={() => dispatch(decrement())} className="border p-2">
+            -
+          </button>
+          <div>{value}</div>
+          <button onClick={() => dispatch(increment())} className="border p-2">
             +
           </button>
         </div>
