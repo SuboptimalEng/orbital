aac3e8f50a4dfff0e80a342a4874f0cba5b599af
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-16 15:59:25 -0500

    Use local functions.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -5,16 +5,24 @@ import './App.css';
 function App() {
   const [count, setCount] = useState(0);
 
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  const decrementCount = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header flex flex-col space-y-4">
         <div className="font-black text-4xl">Learn React</div>
         <div className="flex place-items-center space-x-2">
-          <button onClick={() => setCount(count - 1)} className="border p-2">
+          <button onClick={decrementCount} className="border p-2">
             -
           </button>
           <div>{count}</div>
-          <button onClick={() => setCount(count + 1)} className="border p-2">
+          <button onClick={incrementCount} className="border p-2">
             +
           </button>
         </div>
