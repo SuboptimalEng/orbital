2157853488f36dc78163b8cd392ef8920e5e5481
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-08-16 15:49:40 -0500

    Create React app with TS and Tailwind CSS.

---
+++ app/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="font-black text-4xl">Learn React</div>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
