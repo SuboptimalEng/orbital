79a62fc827a2c9e5c95daa5e88e495a68bd1993c
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-09 17:22:43 -0500

    Test out another way to write the same code.

--- app/src/renderer/components/sidebar/index.tsx
+++ app/src/renderer/components/sidebar/index.tsx
@@ -5,22 +5,31 @@ import { Settings } from './Settings';
 
 const Sidebar = ({ name, icon, isActive }: IActivity) => {
   interface ISidebarDisplays {
-    [key: string]: () => JSX.Element;
+    [key: string]: JSX.Element;
   }
+
+  // NOTE: This works.
+  // interface ISidebarDisplays {
+  //   [key: string]: () => JSX.Element;
+  // }
 
   // NOTE: Conditional rendering is hard to understand.
   // NOTE: Might be worth figuring this out at some point.
   const sidebarDisplays: ISidebarDisplays = {
-    search: () => <Search icon={icon} />,
-    explorer: () => <Explorer icon={icon} />,
-    settings: () => <Settings icon={icon} />,
+    search: <Search icon={icon} />,
+    explorer: <Explorer icon={icon} />,
+    settings: <Settings icon={icon} />,
+    // NOTE: This works.
+    // search: () => <Search icon={icon} />,
+    // explorer: () => <Explorer icon={icon} />,
+    // settings: () => <Settings icon={icon} />,
   };
 
   const sidebarDisplay = sidebarDisplays[name];
 
   return (
     <div className="text-white h-full w-96 border-r-2 border-green">
-      <div className="w-full p-4">{sidebarDisplay()}</div>
+      <div className="w-full p-4">{sidebarDisplay}</div>
     </div>
   );
 };
