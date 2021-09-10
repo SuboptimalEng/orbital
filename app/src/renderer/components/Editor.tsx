b65ea03f6f78d69300c1ad91be67e2b77255b3f2
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-10 13:53:27 -0500

    Refactor code into editor component.

--- app/src/renderer/components/Editor.tsx
+++ app/src/renderer/components/Editor.tsx
@@ -1,7 +1,32 @@
+import { useAppSelector } from '../store/hooks';
+
 const Editor = () => {
+  const { path, files } = useAppSelector((state) => state.folder);
+
   return (
-    <div>
-      <div>editor</div>
+    <div className="m-4">
+      <div className="border p-2 rounder">
+        {path.length > 0 ? path : 'Editor'}
+      </div>
+      <div className="flex flex-row flex-wrap justify-between">
+        {files.map((file: any) => {
+          return (
+            <div
+              className="h-40 w-40 p-2 my-8 border"
+              key={`file-protocol://getMediaFile/${file.path}`}
+            >
+              <div className="border flex flex-col">
+                <img
+                  src={`file-protocol://getMediaFile/${file.path}`}
+                  className="w-40 self-center"
+                  alt=""
+                ></img>
+                <div className="text-xs border p-2">{file.name}</div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- app/src/renderer/components/sidebar/Explorer.tsx
+++ app/src/renderer/components/sidebar/Explorer.tsx
@@ -1,8 +1,4 @@
-import { useAppSelector } from '../../store/hooks';
-
 const Explorer = (props: { icon: string }) => {
-  const { path, files } = useAppSelector((state) => state.folder);
-
   const selectFolder = () => {
     window.ipc.send('select-dirs');
   };
@@ -13,26 +9,6 @@ const Explorer = (props: { icon: string }) => {
       <button className="border p-2 rounded" onClick={() => selectFolder()}>
         Select Folder
       </button>
-      <div className="border p-2 rounder">{path}</div>
-      <div className="flex flex-row flex-wrap justify-between mx-4">
-        {files.map((file: any) => {
-          return (
-            <div
-              className="h-40 w-40 p-2 my-8 border"
-              key={`file-protocol://getMediaFile/${file.path}`}
-            >
-              <div className="border flex flex-col">
-                <img
-                  src={`file-protocol://getMediaFile/${file.path}`}
-                  className="w-40 self-center"
-                  alt=""
-                ></img>
-                <div className="text-xs border p-2">{file.name}</div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
     </div>
   );
 };
