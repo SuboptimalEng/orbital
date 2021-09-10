82b4dbeb54a4b3a1bd16b35a7b57db70c3317914
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-10 16:11:29 -0500

    Minor css changes.

--- app/src/renderer/components/editor/index.tsx
+++ app/src/renderer/components/editor/index.tsx
@@ -11,11 +11,11 @@ const Editor = () => {
         {files.map((file: IFile) => {
           return (
             <div
-              className="flex flex-col "
+              className="flex flex-col border"
               key={`file-protocol://getMediaFile/${file.path}`}
             >
               <video src={`file-protocol://getMediaFile/${file.path}`} />
-              <div className="text-xs p-2">{file.name}</div>
+              <div className="text-xs p-2 border">{file.name}</div>
             </div>
           );
         })}
