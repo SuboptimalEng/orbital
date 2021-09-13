90b04ca4102e878b7cc12b4abe35b625cd8380f5
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-13 11:45:25 -0500

    Refactor grid cols for tailwind css.

--- app/src/renderer/components/editor/index.tsx
+++ app/src/renderer/components/editor/index.tsx
@@ -5,12 +5,31 @@ const Editor = () => {
   const { numOfCols } = useAppSelector((state) => state.settings);
   const { path, files } = useAppSelector((state) => state.folder);
 
+  interface INumOfColsToGridMap {
+    [key: string]: string;
+  }
+
+  const numOfColsToGridMap: INumOfColsToGridMap = {
+    '1': 'grid-cols-1',
+    '2': 'grid-cols-2',
+    '3': 'grid-cols-3',
+    '4': 'grid-cols-4',
+    '5': 'grid-cols-5',
+    '6': 'grid-cols-6',
+  };
+
+  const gridColsClassName = () => {
+    const numOfGridColsStr = numOfColsToGridMap[numOfCols.toString()];
+    const className = `grid ${numOfGridColsStr} gap-8`;
+    return className;
+  };
+
   return (
     <div className="absolute inset-0 overflow-y-auto p-2">
       <div className="border-2 border-green rounded-lg p-2 mb-4">
         {path.length > 0 ? path : 'Editor'}
       </div>
-      <div className={`grid grid-cols-${numOfCols} gap-8`}>
+      <div className={gridColsClassName()}>
         {files.map((file: IFile) => {
           return (
             <div
