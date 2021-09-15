7250ab67d2931139773b5c333543f1912b6da9fb
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-15 15:40:18 -0500

    Testing out typescript.

---
+++ app/src/background/f.js
@@ -0,0 +1,12 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+module.exports = {
+  add,
+  subtract,
+};
--- app/src/background/f.ts
+++
@@ -1,3 +0,0 @@
-export default function a() {
-  console.log('hi from f.ts');
-}
