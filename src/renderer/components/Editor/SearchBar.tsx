efbce76a4278d0a09cbae87097d7dad3a831b95b
suboptimaleng <suboptimaleng@gmail.com>
2021-09-29 19:05:58 -0500

    Update searchbar UI.

--- src/renderer/components/Editor/SearchBar.tsx
+++ src/renderer/components/Editor/SearchBar.tsx
@@ -6,21 +6,21 @@ const SearchBar = () => {
   const dispatch = useAppDispatch();
 
   return (
-    <div className="bg-gray-600 h-24 px-16 shadow-2xl">
-      <div className=" h-full flex place-items-center space-x-2 text-base text-activity-fg">
+    <div className="bg-activity-bg text-activity-fg h-24 px-16 shadow-2xl">
+      <div className=" h-full flex place-items-center space-x-2 ">
         <input
           type="text"
           value={query}
           placeholder="Search files by name"
           onChange={(e) => dispatch(setQuery(e.target.value))}
-          className="bg-activity-bg border border-activity-fg rounded-lg text-activity-fg px-4 py-2 focus:outline-none"
+          className="bg-activity-bg w-full py-2 focus:outline-none"
         />
-        <button
+        {/* <button
           onClick={() => dispatch(toggleSortByName())}
           className="border border-activity-fg px-2 rounded-lg"
         >
           Sort by Name
-        </button>
+        </button> */}
       </div>
     </div>
   );
