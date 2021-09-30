f654ac4e95c2d5931fc2390101227bd6760d6b5c
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-09-30 12:33:51 -0500

    hm

---
+++ src/renderer/types/index.js
@@ -0,0 +1,10 @@
+"use strict";
+// ================================================================
+// Slices
+// ================================================================
+exports.__esModule = true;
+exports.Activities = void 0;
+exports.Activities = {
+    Explorer: 'explorer',
+    Settings: 'settings'
+};
