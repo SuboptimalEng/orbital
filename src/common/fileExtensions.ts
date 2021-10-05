63c85d1c8f1a295192843a5729f3ea2f37745f9c
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-10-05 16:24:56 -0500

    Handle ogg files in v2.

--- src/common/fileExtensions.ts
+++ src/common/fileExtensions.ts
@@ -1,8 +1,9 @@
 // TODO V2: Figure out how to display these types of files.
 // 'mpg', // could be opened, but not working in preview
+// 'ogg', // could be opened, but not working in preview
 // 'avi', // could not be opened on mac
 // 'wmv', // could not be opened on mac
-const videoExtensions: Array<string> = ['mov', 'mp4', 'webm', 'ogg'];
+const videoExtensions: Array<string> = ['mov', 'mp4', 'webm'];
 
 // TODO V2: Figure out how to display all types of image files.
 // 'icns',
