6a86b985b5909e1e8bae3c20ff4e3c4c2b3303db
Suboptimal Engineer <suboptimaleng@gmail.com>
2021-10-13 12:12:03 -0500

    Remove outline on video focus.

--- src/renderer/components/Editor/Explorer/VideoPreview.tsx
+++ src/renderer/components/Editor/Explorer/VideoPreview.tsx
@@ -6,7 +6,7 @@ export default function VideoPreview({ path }: IFile) {
       id={path}
       typeof="video/ogg"
       src={`file-protocol://getMediaFile/${path}`}
-      className="object-contain max-w-full max-h-full bg-card-bg"
+      className="object-contain max-w-full max-h-full bg-card-bg focus:outline-none"
       controls
     />
   );
